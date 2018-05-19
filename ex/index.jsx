a0a71416fa78da67dff69cdac0d919523d001c16
rodrigoluan <rodrigoluanx@gmail.com>
2018-05-19 14:15:30 -0300

    added clone of the components

--- ex/index.jsx
+++ ex/index.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import SilvaFamily from './silvaFamily'
+import Family from './family'
+import Member from './member'
 
 
 ReactDOM.render(
-    <SilvaFamily />
+    <Family lastName='Silva'>
+        <Member name='Rodrigo' />
+        <Member name='Karina' />
+        <Member name='Genivaldo' />
+    </Family>
     ,document.getElementById('app'))
--- ex/member.jsx
+++ ex/member.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
 
-export default (props) => (
+export default props => (
     <div> {props.name} <strong> {props.lastName} </strong> </div>
 )
